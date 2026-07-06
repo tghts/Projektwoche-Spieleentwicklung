d4ed35729b94670aef6255cda4018e86f50cf9d6
tghts <69969100+tghts@users.noreply.github.com>
2026-07-06 13:23:45 +0200

    CRLF

--- Scene/main_menu.gd
+++ Scene/main_menu.gd
@@ -3,10 +3,9 @@ extends Node
 
 func _on_startknopf_pressed() -> void:
 	click_02.play()
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.2).timeout #wait-methode für 0.2 sekunden
 	get_tree().change_scene_to_file("res://Scene/level_1.tscn")
 	
-
 
 func _on_exitknopf_pressed() -> void:
 	get_tree().quit()
