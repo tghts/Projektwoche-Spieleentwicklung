af5ccb05c06d28c0415b74ef620700abc41823b3
tghts <69969100+tghts@users.noreply.github.com>
2026-07-06 16:52:06 +0200

    fixed exit
    yeh it works again

--- Scene/main_menu.gd
+++ Scene/main_menu.gd
@@ -6,8 +6,7 @@ func _on_startknopf_pressed() -> void:
 	await get_tree().create_timer(0.2).timeout #wait-methode für 0.2 sekunden
 	get_tree().change_scene_to_file("res://Scene/level_1.tscn")
 
-func _on_exitknopf_pressed() -> void:
-	get_tree().quit()
+
 
 
 
