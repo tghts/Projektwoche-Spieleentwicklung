bc0a0d4484a5334983bedfa7f2f0766f52d97857
tghts <69969100+tghts@users.noreply.github.com>
2026-07-07 13:56:37 +0200

    addition of the pause screen

--- Script/character.gd
+++ Script/character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var healt_points:int = 3
-const SPEED = 300.0
+const SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 var jump_counter = 0
 
