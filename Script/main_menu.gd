ee2cb82a5ca05496f6f73dc8f0fb9d1d9f782766
S <146875334+vailim@users.noreply.github.com>
2026-07-07 21:04:56 +0200

    add 404 button functionality

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -1,5 +1,6 @@
 extends Node
 @onready var click: AudioStreamPlayer = $AudioStreamPlayer/Click
+@onready var sphinx: Control = $Sphinx
 
 func _ready():
 	$Startknopf.grab_focus()
@@ -8,13 +9,17 @@ func _on_startknopf_pressed() -> void:
 	click.play()
 	await get_tree().create_timer(0.2).timeout #wait-methode für 0.2 sekunden
 	get_tree().change_scene_to_file("res://Scene/level_1.tscn")
-	
 
 func _on_exitknopf_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_settingknopf_pressed() -> void:
 	click.play()
 	await get_tree().create_timer(0.2).timeout #wait-methode für 0.2 sekunden
 	get_tree().change_scene_to_file("res://Scene/settings.tscn")
+
+func _on_abc_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		sphinx.show()
+	else:
+		sphinx.hide()
