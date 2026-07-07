5c6a760b778c8bedc60795d7e578425ab5a42066
Linuxuser41230 <mamaslucas056@gmail.com>
2026-07-07 09:31:18 +0200

    hhh

--- Scene/in_game_ui.gd
+++ Scene/in_game_ui.gd
@@ -2,6 +2,14 @@ extends Node
 
 @onready var character: CharacterBody2D = %Character
 
-var lives:int = 3
+var lives:int = 2
 
 @export var hearts : Array[Node]
+func _process(delta: float) -> void:
+	display_lives()
+	
+func display_lives():
+	for n in lives:
+		hearts[n].show()
+func dectrease_health():
+	lives -= 1 
--- Script/enemy.gd
+++ Script/enemy.gd
@@ -2,5 +2,14 @@ class_name Enemy
 
 extends RigidBody2D
 
+@onready var in_game_ui: Node = %InGameUI
+
+
 @export var health_ponits: int = 3 
 @export var atack_damage: int = 1  
+
+
+
+func _on_damage_zone_body_entered(body: Node2D) -> void:
+	in_game_ui.dectrease_health()
+	print("hall")
