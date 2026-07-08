dcbecde7e9de8053b84a21274db8fb65c895e14b
Linuxuser41230 <mamaslucas056@gmail.com>
2026-07-08 13:54:12 +0200

    Sprintatack

--- Script/enemy.gd
+++ Script/enemy.gd
@@ -17,3 +17,7 @@ func _process(_delta: float) -> void:
 	if (character.position.x < position.x):
 		position.x -= 3
 		animated_sprite_2d.flip_h = false
+
+func sprint_angriff():
+	animated_sprite_2d.frame = 30
+	
