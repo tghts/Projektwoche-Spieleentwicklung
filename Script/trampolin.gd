a564918c390f68f1f153bcc1d2447335cad653ff
S <146875334+vailim@users.noreply.github.com>
2026-07-08 11:40:25 +0200

    add sound

--- Script/trampolin.gd
+++ Script/trampolin.gd
@@ -1,7 +1,7 @@
 extends Area2D
-
-
+@onready var trampolin: AudioStreamPlayer = $AudioStreamPlayer/Trampolin
 
 func _on_body_entered(body: Node2D) -> void:
+	trampolin.play()
 	if (body.name == "Character"):
 		body.jump()
