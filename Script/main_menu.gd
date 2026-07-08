be0f0904d7fde08c249c730fe591bf297d314279
S <146875334+vailim@users.noreply.github.com>
2026-07-08 08:56:34 +0200

    improve ui sound effects

--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -1,5 +1,7 @@
 extends Node
 @onready var click: AudioStreamPlayer = $AudioStreamPlayer/Click
+@onready var appear: AudioStreamPlayer = $AudioStreamPlayer/Appear
+@onready var disappear: AudioStreamPlayer = $AudioStreamPlayer/Disappear
 @onready var sphinx: Control = $Sphinx
 
 func _ready():
@@ -7,19 +9,23 @@ func _ready():
 
 func _on_startknopf_pressed() -> void:
 	click.play()
-	await get_tree().create_timer(0.2).timeout #wait-methode für 0.2 sekunden
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://Scene/level_1.tscn")
 
 func _on_exitknopf_pressed() -> void:
+	click.play()
+	await get_tree().create_timer(0.1).timeout
 	get_tree().quit()
 
 func _on_settingknopf_pressed() -> void:
 	click.play()
-	await get_tree().create_timer(0.2).timeout #wait-methode für 0.2 sekunden
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://Scene/settings.tscn")
 
 func _on_abc_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		sphinx.show()
+		appear.play()
 	else:
 		sphinx.hide()
+		disappear.play()
--- Script/settings.gd
+++ Script/settings.gd
@@ -1,5 +1,7 @@
 extends Node
 @onready var click: AudioStreamPlayer = $AudioStreamPlayer/Click
+@onready var music_slider: AudioStreamPlayer = $AudioStreamPlayer/MusicSlider
+@onready var sound_effects_slider: AudioStreamPlayer = $AudioStreamPlayer/SoundEffectsSlider
 @onready var music: HSlider = $Music
 @onready var sound_effects: HSlider = $SoundEffects
 
@@ -14,10 +16,11 @@ func _unhandled_input(event):
 
 func _on_back_pressed() -> void:
 	click.play()
-	await get_tree().create_timer(0.2).timeout # wait-methode für 0.2 sekunden
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
 
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	click.play()
 	if toggled_on:
 		fullscreen()
 	else:
@@ -29,12 +32,14 @@ func fullscreen() -> void:
 func windowed() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
-func _on_sound_effects_value_changed(value: float) -> void:
-	GameData.sound_effects_volume = value
-	var bus_index := AudioServer.get_bus_index("SoundEffects")
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
-
 func _on_music_value_changed(value: float) -> void:
+	music_slider.play()
 	GameData.music_volume = value
 	var bus_index := AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+
+func _on_sound_effects_value_changed(value: float) -> void:
+	sound_effects_slider.play()
+	GameData.sound_effects_volume = value
+	var bus_index := AudioServer.get_bus_index("SoundEffects")
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
