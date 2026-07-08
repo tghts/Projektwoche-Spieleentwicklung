5a4db9e1087d290ea1fb0cf11bc4da4a5e772fbd
Linuxuser41230 <mamaslucas056@gmail.com>
2026-07-08 11:32:39 +0200

    Level1

---
+++ Scene/deathzone.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if(body.name == "Character"):
+		get_tree().reload_current_scene()
--- Script/portal.gd
+++ Script/portal.gd
@@ -3,4 +3,5 @@ extends Area2D
 @export var target_level : PackedScene
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_packed.call_deferred(target_level)
+	if (body.name == "Character"):
+		get_tree().change_scene_to_packed.call_deferred(target_level)
