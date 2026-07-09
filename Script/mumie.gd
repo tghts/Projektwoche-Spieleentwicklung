80d5fde49ff16770c48a8e135710e236b9b2d458
Linuxuser41230 <mamaslucas056@gmail.com>
2026-07-09 14:03:03 +0200

    CleanUp and Setting background

--- Script/mumie.gd
+++ Script/mumie.gd
@@ -2,9 +2,10 @@ extends Enemy
 
 @onready var character: CharacterBody2D = $"../../Character"
 
+
+
 func _process(_delta: float) -> void:
-	
-	animated_sprite_2d.animation = "walk"
+
 	if (character.position.x > position.x):
 		velocity.x = 200
 		animated_sprite_2d.flip_h = true
@@ -17,6 +18,8 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if (is_on_floor()):
+		animated_sprite_2d.animation = "walk"
 	move_and_slide()
 
 
