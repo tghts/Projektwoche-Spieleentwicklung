4f9b202e99363b41465ec26f8addb7c4115a31df
Linuxuser41230 <mamaslucas056@gmail.com>
2026-07-09 09:47:57 +0200

    dff

--- Script/dragon.gd
+++ Script/dragon.gd
@@ -1,17 +1,21 @@
 extends CharacterBody2D
 
+@onready var character: CharacterBody2D = $"../Character"
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var firebreath: PackedScene = preload("res://Scene/fire_breath.tscn")
 
-func _physics_process(delta: float) -> void:
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	move_and_slide()
+func _process(delta: float) -> void:
+	animated_sprite_2d.animation = "flying"
+	if (character.position.x > position.x):
+		velocity.x = 200
+		animated_sprite_2d.flip_h = true
+		
+	if (character.position.x < position.x):
+		velocity.x = -200
+		animated_sprite_2d.flip_h = false
 	
 func breathfire():
 	var fire:= firebreath.instantiate() as Node2D
--- Script/mumie.gd
+++ Script/mumie.gd
@@ -25,4 +25,4 @@ func _on_damage_zone_body_entered(body: Node2D) -> void:
 	if (body.name =="Character"):
 		print("Du hast schaden bekommen")
 		GameData.reduce_character_hp()
-		body.jump_side(velocity.x*10)
+		body.jump_side(velocity.x*5)
