4265b75ff5887870423a973591a69cba00a1b8c8
S <146875334+vailim@users.noreply.github.com>
2026-07-09 09:43:58 +0200

    fix game over screen

--- Script/game_over_screen.gd
+++ Script/game_over_screen.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-
 func _ready() -> void:
-	pass
+	$Panel/VBoxContainer/StartAgain.grab_focus()
 
 func _on_start_again_pressed() -> void:
+	SoundManager.play("click")
 	get_tree().reload_current_scene()
 
-
 func _on_back_to_menu_pressed() -> void:
+	SoundManager.play("click")
 	get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
--- Script/spikes.gd
+++ Script/spikes.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-
-
 func _on_body_entered(body: Node2D) -> void:
 	SoundManager.play("hit")
 	GameData.reduce_character_hp()
